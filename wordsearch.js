6bbbe261610b4b88e915623e8bc0fb2792d6e10d
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-06-30 20:17:02 +0000

    completed the wordSearch function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,22 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map((ls) => ls.join(""));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+      if (l.includes(word)) return true;
     }
-}
+  
+    const vertical = letters[0].map((_, columnIndex) =>
+      letters.map((row) => row[columnIndex]).join(""),
+    );
+    for (row of vertical) {
+      if (row.includes(word)) return true;
+    }
+  
+    if (undefined) {
+      return false;
+    }
 
-module.exports = wordSearch
+    return false;
+};
+  
+  module.exports = wordSearch;
+  
