7c58070a154d98e888e4d8c1e5f28959ffa079f5
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-06-30 20:21:13 +0000

    linted and refactored the function

--- wordsearch.js
+++ wordsearch.js
@@ -1,22 +1,23 @@
 const wordSearch = (letters, word) => {
-    const horizontalJoin = letters.map((ls) => ls.join(""));
-    for (l of horizontalJoin) {
-      if (l.includes(word)) return true;
-    }
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
   
-    const vertical = letters[0].map((_, columnIndex) =>
-      letters.map((row) => row[columnIndex]).join(""),
-    );
-    for (row of vertical) {
-      if (row.includes(word)) return true;
-    }
+  const vertical = letters[0].map((_, columnIndex) =>
+    letters.map((row) => row[columnIndex]).join(""),
+  );
+    
+  for (row of vertical) {
+    if (row.includes(word)) return true;
+  }
   
-    if (undefined) {
-      return false;
-    }
-
+  if (undefined) {
     return false;
+  }
+
+  return false;
 };
   
-  module.exports = wordSearch;
+module.exports = wordSearch;
   
